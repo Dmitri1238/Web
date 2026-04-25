808bf0c488574216ac58567dee97b0e508322c4f
Dmitri1238 <0programme0@gmail.com>
2026-04-25 16:33:27 +0300

    deploy

--- my_django_project/urls.py
+++ my_django_project/urls.py
@@ -1,7 +1,10 @@
 # my_django_project/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 from main import views
+
 
 urlpatterns = [
     path('admin/payment-request/<int:request_id>/approve/', views.approve_payment_request, name='approve_payment_request'),
@@ -9,4 +12,8 @@ urlpatterns = [
     path('admin/requests/', views.admin_payment_requests_view, name='admin_payment_requests'),  # Ваш маршрут
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
