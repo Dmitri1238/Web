f95a9a1392f2334b319e263503dbb0c6d5682d1d
Dmitri1238 <0programme0@gmail.com>
2026-04-25 21:17:11 +0300

    add cloudinary

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -45,15 +45,6 @@ INSTALLED_APPS = [
     'main',
 ]
 
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -139,11 +130,17 @@ LANGUAGE_CODE = 'ru'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 if os.environ.get("CLOUD_NAME"):
-    cloudinary.config(
-        cloud_name=os.environ.get("CLOUD_NAME"),
-        api_key=os.environ.get("API_KEY"),
-        api_secret=os.environ.get("API_SECRET"),
-    )
+    CLOUDINARY_STORAGE = {
+        "CLOUD_NAME": os.environ.get("CLOUD_NAME"),
+        "API_KEY": os.environ.get("API_KEY"),
+        "API_SECRET": os.environ.get("API_SECRET"),
+    }
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+}
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
