89d68fc1324229aac0fc6137a8a48274235c631d
Dmitri1238 <0programme0@gmail.com>
2026-04-25 16:17:45 +0300

    add category admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from main.models import Book
 from main.models import UserProfile
+from main.models import Category
 
 admin.site.register(Book)
 @admin.register(UserProfile)
@@ -8,3 +9,8 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'status', 'avatar')  # показывать поля
     search_fields = ('user__username',)
     list_filter = ('status',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {"slug": ("name",)}
